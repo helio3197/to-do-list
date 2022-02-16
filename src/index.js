5cc22a058f39d5650b61f8b1d7acc99c459692f5
Kenny <kennysalazar3197@gmail.com>
2022-02-15 20:16:06 -0500

    Created js functions to display the to-do list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import createTodo from './create-tasks-list.js';
 import './style.css';
+
+const taskList = [
+  {
+    description: 'test-task',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'test-task2',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'test-task3',
+    completed: true,
+    index: 3,
+  },
+];
+
+const todoList = document.getElementById('todo-list');
+
+createTodo(taskList, todoList);
