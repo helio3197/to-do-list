b8ff574202ab829d22f8b4c37c058a0665475f2f
Ivan Silva <ivankuartz@hotmail.com>
2022-02-23 14:49:22 -0300

    Added first version of remove tast test

--- src/task-list-features.test.js
+++ src/task-list-features.test.js
@@ -16,17 +16,29 @@ const addTask = (list, description) => {
 const removeTask = (list, index) => {
   list.splice(index, 1);
   list = sortTaskList(list);
-  localStorage.setItem('taskList', JSON.stringify(list));;
+  localStorage.setItem('taskList', JSON.stringify(list));
 
   return list;
 };
 
+const filledArray = [{ description: 'New Test', completed: false, index: 1 }];
+
 describe('Tests for the addTask function:', () => {
-  test('Test output: Calling addTask([], "New Test") should return [{description: "New Test", completed: false, index: 1}]', () => {
-    expect(addTask([], 'New Test')).toStrictEqual([{ description: 'New Test', completed: false, index: 1 }]);
+  test(`Test input: Calling addTask([], "New Test") should return ${filledArray}`, () => {
+    expect(addTask([], 'New Test')).toStrictEqual(filledArray);
   });
 
   test('Test LocalStorage: Calling addTask([], "New Test") and running localStorage.getItem("taskList") should return "[{description: "New Test", completed: false, index: 1}]"', () => {
     expect(localStorage.getItem('taskList')).toBe('[{"description":"New Test","completed":false,"index":1}]');
+  });
+});
+
+describe('Tests for the removeTask function:', () => {
+  test(`Test input: Calling removeTask(${filledArray},1) and running localStorage.getItem("taskList") should return "[]"`, () => {
+    expect(removeTask(filledArray, 1)).toStrictEqual('[]');
+  });
+
+  test(`Testing Local Storage:Calling removeTask(${filledArray},1) and running localStorage.getItem("taskList") should return "[]"`, () => {
+    expect(localStorage.getItem('taskList')).toBe('[]');
   });
 });
