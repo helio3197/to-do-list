133d2863994a9e2d0c5541f0ae240b332dc60749
Kenny <kennysalazar3197@gmail.com>
2022-02-23 12:12:54 -0500

    Added unit-test group  to the addTask test file

--- src/task-list-features.test.js
+++ src/task-list-features.test.js
@@ -1,10 +1,23 @@
-import { addTask } from './task-list-features.js';
 import localStorage from './__mocks__/localStorage.js';
 
-const saveListToStorage = (list) => localStorage.setItem('taskList', JSON.stringify(list));
+const addTask = (list, description) => {
+  const newtask = {
+    description,
+    completed: false,
+    index: list.length + 1,
+  };
+  list.push(newtask);
+  localStorage.setItem('taskList', JSON.stringify(list));
+
+  return list;
+};
 
 describe('Tests for the addTask function:', () => {
-  test('Calling addTask([], "New Test") should return [{description: "New Test", completed: false, index: 1}]', () => {
-    expect(addTask([], 'New Test')).toBe([{ description: 'New Test', completed: false, index: 1 }]);
+  test('Test output: Calling addTask([], "New Test") should return [{description: "New Test", completed: false, index: 1}]', () => {
+    expect(addTask([], 'New Test')).toStrictEqual([{ description: 'New Test', completed: false, index: 1 }]);
+  });
+
+  test('Test LocalStorage: Calling addTask([], "New Test") and running localStorage.getItem("taskList") should return "[{description: "New Test", completed: false, index: 1}]"', () => {
+    expect(localStorage.getItem('taskList')).toBe('[{"description":"New Test","completed":false,"index":1}]');
   });
 });
